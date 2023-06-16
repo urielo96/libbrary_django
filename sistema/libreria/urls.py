9d5ace45538a433311c2a0ef4b3213b23bfb604b
Urielo <diegouriel96@hotmail.com>
2023-06-15 18:40:34 -0600

    Ya un gran avance

--- sistema/libreria/urls.py
+++ sistema/libreria/urls.py
@@ -5,5 +5,12 @@ from . import views
 urlpatterns = [
     path('',views.inicio,name = 'inicio'),
     path("mi_pagina/", views.mi_pagina, name="mi_pagina"),
-    path("libros",views.libros , name="libros")
+    path("libros",views.libros , name="libros"),
+    path("libros/crear",views.crear , name="crear"),
+    path("(libros/editar",views.editar , name="editar"),
+    
+
+
+   
+    
     ]
--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
-    return HttpResponse("<h1>Bienvenido a la libreria</h1>")
+    return render(request,"paginas/inicio.html")
 
 def mi_pagina(request):
     return render(request,"paginas/mi_pagina.html")
 
 def libros(request):
     return render(request,"libros/index.html")
+
+def crear(request):
+    return render(request,"libros/crear.html")
+
+def editar(request):
+    return render(request,"libros/editar.html")
