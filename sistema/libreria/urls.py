ca91e046a61b13c9cf0452438685a91d63beafbf
Urielo <diegouriel96@hotmail.com>
2023-06-15 21:22:41 -0600

    Aqui ya se agrago todas las imagenes y con formulario

--- sistema/libreria/urls.py
+++ sistema/libreria/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
 
 urlpatterns = [
     path('',views.inicio,name = 'inicio'),
@@ -9,8 +12,5 @@ urlpatterns = [
     path("libros/crear",views.crear , name="crear"),
     path("(libros/editar",views.editar , name="editar"),
     
-
-
-   
-    
-    ]
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+ 
