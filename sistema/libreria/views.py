1db0d156070027b0dace23a688551b6deeeb8439
diego <diegouriel96.com>
2023-06-16 18:41:22 -0600

    Aqui ya se puede editar un libro

--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Libro
 from .forms import LibroForm
@@ -15,8 +15,19 @@ def libros(request):
     
 
 def crear(request):
-    formulario = LibroForm(request.POST or None)
+    formulario = LibroForm(request.POST or None,request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('libros')
     return render(request,"libros/crear.html",{'formulario':formulario})
 
-def editar(request):
-    return render(request,"libros/editar.html")
+def editar(request, id):
+    libro = Libro.objects.get(id = id)
+    print(libro)
+    formulario = LibroForm(request.POST or None,request.FILES or None, instance=libro)
+    return render(request,"libros/editar.html",{'formulario':formulario})
+
+def eliminar(request,id):
+    libro = Libro.objects.get(id = id)
+    libro.delete()
+    return redirect('libros')
